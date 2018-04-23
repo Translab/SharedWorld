7c702569e22a944f9cdab9779a518daedbb41b19
Jing Yan <jing@ResNet-6-148.resnet.ucsb.edu>
2018-04-23 01:49:12 -0700

    a collage piece, a morph object, a stroke

---
+++ Assets/Imports/Jing/# scripts/blend_shape.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blend_shape : MonoBehaviour {
+	int blendShapeCount;
+	SkinnedMeshRenderer skinnedMeshRenderer;
+	Mesh skinnedMesh;
+	float blendOne = 0f;
+	float blendTwo = 0f;
+	public float blendSpeed = 1f;
+	bool blendOneFinished = false;
+
+	void Awake ()
+	{
+		skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer> ();
+		skinnedMesh = GetComponent<SkinnedMeshRenderer> ().sharedMesh;
+	}
+
+	// Use this for initialization
+	void Start () {
+		blendShapeCount = skinnedMesh.blendShapeCount; 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		blendOne += blendSpeed;
+		skinnedMeshRenderer.SetBlendShapeWeight (0, blendOne);
+		if (blendOne > 100f || blendOne < 0.0f) {
+			blendSpeed = -blendSpeed;
+		}
+
+	}
+}
