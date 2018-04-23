6fd9bd23909ff12477d1fded1908a87f08ed531d
GustavoAlfonsoRincon <gustavo.a.rincon@gmail.com>
2018-04-23 09:17:14 -0700

    aGRAdjustedComposition
    Adjusted the composition for scale.

---
+++ Assets/Imports/Gustavo/Model_Head/HeadBehaviourScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadSpinScript : MonoBehaviour {
+	public float speed = 2f;
+
+	void Update() {
+		transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+	}
+}
