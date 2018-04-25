e4bc932810f91ca88dd2829b5a7552baa6d57767
mengyuchen <mengyuchen@umail.ucsb.edu>
2018-04-25 15:32:46 -0700

    mengyu_weidi_import

---
+++ Assets/Imports/Weidi/export/weidi_stamp/MOVINGdown.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MOVINGdown: MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    public float countdown = 20f;
+
+
+    void Update()
+    {
+        countdown -= Time.deltaTime*10;
+        if (countdown >= 0.0f)
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime); ;
+        }
+        if (countdown <= -20.0f)
+            countdown = 20.0f;
+        print(countdown);
+    }
+}
