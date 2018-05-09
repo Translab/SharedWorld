52a6be5a8a48824c8f9ee2a0816c4b011abff0ec
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2018-05-09 00:24:03 -0700

    Zhenyu: Added PID controller

---
+++ Assets/Imports/Zhenyu/scripts/PID.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PID{
+
+	float reference;
+	float error_p;
+	float error_p_prev;
+	float error_i;
+	float error_d;
+	float p;
+	float i;
+	float d;
+
+	bool isOperational;
+
+	public PID(float p, float i, float d){
+		this.p = p;
+		this.i = i;
+		this.d = d;
+		error_p_prev = 0;
+		isOperational = false;
+	}
+
+	public void setReference(float reference){
+		this.reference = reference;
+		isOperational = true;
+	}
+
+	public float process(float dataIn){
+		error_p = reference - dataIn;
+
+		if(Mathf.Abs(error_p)<0.0001f){
+			isOperational = false;
+		}
+
+
+		error_i+=error_p;
+		error_d = error_p - error_p_prev;
+
+		error_p_prev = error_p;
+
+		return (p*error_p)+(i*error_i)+(d*error_d);
+	}
+
+	public bool isOn(){
+		return isOperational;
+	}
+
+}
