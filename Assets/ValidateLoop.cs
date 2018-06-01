068a8799861efca6647fec6f87bd03384e0e3d73
mengyuchen <mengyuchen@ucsb.edu>
2018-06-01 13:05:52 -0700

    eoys_attemptBuild

---
+++ Assets/ValidateLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValidateLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void OnValidate() {
+		 GameObject[] allGameObjects = FindObjectsOfType<GameObject>();
+ 			foreach (GameObject go in allGameObjects)
+ 		{
+     	go.hideFlags = HideFlags.None;
+     	Component[] components = go.GetComponents(typeof(Component));
+     	for (int i = 0; i < components.Length; i++)
+     	{
+         components[i].hideFlags = HideFlags.None;
+     	}
+		}
+	}
+	
+	
+}
